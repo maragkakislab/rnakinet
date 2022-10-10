6afb134d40372129344796cec2863b3d147fefba
MartinekV <martinekvlastimil95@gmail.com>
2022-10-10 19:10:17 +0000

    Add iterative paralelizable validation dataset

--- dataloading.py
+++ dataloading.py
@@ -13,6 +13,8 @@ from ont_fast5_api.fast5_interface import get_fast5_file
 from scipy import stats
 from datamap import experiment_files
 from bonito_pulled.bonito.reader import trim
+import torch
+import math
 
 def process_fast5_read(read, window, skip=1000, zscore=True, smartskip = True):
     """ Normalizes and extracts specified region from raw signal """
@@ -214,48 +216,103 @@ def get_my_dataset(window=1000, pos_files = 'pos_2022', neg_files='neg_2022', va
     return train, MyMixedDatasetValidStatic(valid, valid_limit)
     
 
-def get_my_valid_dataset(window=1000, pos_files = 'pos_2022', neg_files='neg_2022', valid_limit=1000, valid_files_count=3, valid_select_seed=42):
-    pos_files = sorted(experiment_files[pos_files])
-    neg_files = sorted(experiment_files[neg_files])
+def get_my_valid_dataset_unlimited(window=1000, pos_files = 'pos_2022', neg_files='neg_2022', split_method=get_default_split):
+    #IN PROGRESS
+    split = split_method(pos_files=pos_files, neg_files=neg_files)
     
-    #TODO previous validation = last 3 files
-    seed = valid_select_seed
-    deterministic_random = random.Random(seed)
-    deterministic_random.shuffle(pos_files)
-    deterministic_random.shuffle(neg_files)
-    
-    train_pos_files = pos_files[:-valid_files_count]
-    train_neg_files = neg_files[:-valid_files_count]
-    valid_pos_files = pos_files[-valid_files_count:]
-    valid_neg_files = neg_files[-valid_files_count:]
-    #always shuffling training data
-    random.shuffle(train_pos_files)
-    random.shuffle(train_neg_files)
+    train_pos_files = split['train_pos_files']
+    train_neg_files = split['train_neg_files']
+    valid_pos_files = split['valid_pos_files']
+    valid_neg_files = split['valid_neg_files']
     
     print('valid files indicies')
     for files in [valid_pos_files, valid_neg_files]:
         print(sorted([int(Path(x).stem.split('_')[-1]) for x in files]))
     
-    valid = MyMixedDatasetValid(valid_pos_files, valid_neg_files, window=window, limit=valid_limit, valid_files_count=valid_files_count)
-
-    class MyMixedDatasetValidStatic(Dataset):
-        def __init__(self,generator,limit):
-            self.datapoints = []
-            gen = iter(generator)
-            for i in range(limit):
-                if(i%10 == 0):
-                    print(i)
-                #TODO sample randomly, not from the beginning?
-                self.datapoints.append(next(gen))
-                
-        def __getitem__(self,idx):
-            return self.datapoints[idx]
-        def __len__(self):
-            return len(self.datapoints)
     
-    return MyMixedDatasetValidStatic(valid, valid_limit)
+    def process_fast5_read_full(read, window):
+        """ Normalizes and extracts specified region from raw signal """
+
+        s = read.get_raw_data(scale=True)  # Expensive
+        s = stats.zscore(s)
+
+        #Using custom trim arguments according to Explore notebook
+        skip, _ = my_trim(signal=s[:26000])
+
+        last_start_index = len(s)-window
+        if(last_start_index < skip):
+            # if sequence is not long enough, last #window signals is taken, ignoring the skip index
+            skip = last_start_index
+        s = s[skip:]
+        
+        return s
+    
+    def myite_full(files, label, window):
+        for fast5 in files:
+            # yield (Path(fast5).stem + f'_lab_{label}')
+            # print(Path(fast5).stem,'starting')
+            with get_fast5_file(fast5, mode='r') as f5:
+                for i, read in enumerate(f5.get_reads()):
+                    # print(Path(fast5).stem, 'READ',i, 'TODO DELETE PRINT')
+                    x = process_fast5_read_full(read, window)
+                    y = np.array(label)
+                    for start in range(0, len(x), window)[:-1]: #Cutoff the last incomplete signal
+                        stop = start+window
+                        identifier = {'file':str(fast5),
+                                      'readid':read.read_id,
+                                      'read_index_in_file':i,
+                                      'start':start,
+                                      'stop':stop,
+                                      'label':label
+                                     }
+                        
+                        yield x[start:stop].reshape(-1,1).swapaxes(0,1), np.array([y], dtype=np.float32), identifier
+                        
+    
+    def mixed_generator_full(positive_files, negative_files, window):
+        pos_gen = myite_full(positive_files, 1, window)
+        neg_gen = myite_full(negative_files, 0, window)
+        for pos in pos_gen:
+            yield pos
+        for neg in neg_gen:
+            yield neg
+    
+    class FullValidDataset(IterableDataset):
+        def __init__(self, positive_files, negative_files, window):
+            self.positive_files = positive_files
+            self.negative_files = negative_files
+            self.window = window
+
+        def __iter__(self):
+            return mixed_generator_full(self.positive_files, self.negative_files, self.window)
+    
+    return FullValidDataset(valid_pos_files, valid_neg_files, window=window)
+
+def worker_init_fn(worker_id):
+    worker_info = torch.utils.data.get_worker_info()
+    dataset = worker_info.dataset
+    total_workers = worker_info.num_workers
+    current_worker = worker_id
+    
+    pos_per_worker = len(dataset.positive_files)//total_workers
+    neg_per_worker = len(dataset.negative_files)//total_workers
+    
+    if(current_worker == total_workers -1): #Last worker
+        # print('LAST WORKER')
+        dataset.positive_files = dataset.positive_files[pos_per_worker*current_worker:]
+        dataset.negative_files = dataset.negative_files[neg_per_worker*current_worker:]
+    else:
+        dataset.positive_files = dataset.positive_files[pos_per_worker*current_worker: pos_per_worker*(current_worker+1)]
+        dataset.negative_files = dataset.negative_files[neg_per_worker*current_worker: neg_per_worker*(current_worker+1)] 
+       
+    # print(sorted([int(Path(x).stem.split('_')[-1]) for x in dataset.positive_files]))
+    # print(sorted([int(Path(x).stem.split('_')[-1]) for x in dataset.negative_files]))
+    
+    assert(len(dataset.positive_files)>0), f'{pos_per_worker*current_worker}: {pos_per_worker*(current_worker+1)}'
+    assert(len(dataset.negative_files)>0), f'{neg_per_worker*current_worker}: {neg_per_worker*(current_worker+1)}'
 
 
+                         
 def my_trim(signal, window_size=200, threshold=1.9, min_elements=25):
 
     min_trim = 10
