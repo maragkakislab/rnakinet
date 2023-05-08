46e01761d0bcd68f2da670dfc5c70775b04adfb1
MartinekV <martinekvlastimil95@gmail.com>
2023-05-08 15:17:59 +0000

    Cleanup setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
-# !git clone https://github.com/hsd1503/resnet1d.git
-# https://github.com/maxjcohen/transformer.git
 # !pip instal -e .
 setup(name='rnamodif',
       version='1.0',
       packages=find_packages(),
-      # py_modules=['RNAModif'],
 )
