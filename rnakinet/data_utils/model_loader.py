52d70a149cfad5d6128b9b82d6e6d4786ac5ca4f
Anirudh Nangunoori <anirudh.nangunoori@nih.gov>
2026-03-26 18:36:42 -0400

    model_loader.py: formatting

--- rnakinet/data_utils/model_loader.py
+++ rnakinet/data_utils/model_loader.py
@@ -3,23 +3,22 @@ from rnakinet.models.model import RNAkinet
 from rnakinet.models.model_experimental import RNAkinet_LastOnly
 from rnakinet.models.model_r9 import RNAkinet as RNAkinet_r9
 
-arch_map = {
-    'rnakinet':RNAkinet,
-    'rnakinet_lastonly': RNAkinet_LastOnly,
-    'rnakinet_r9': RNAkinet_r9,
-}
+arch_map = {'rnakinet': RNAkinet,
+            'rnakinet_lastonly': RNAkinet_LastOnly,
+            'rnakinet_r9': RNAkinet_r9
+            }
+
 default_models = {
-    'rnakinet_r9_5EU' : {
+    'rnakinet_r9_5EU': {
         # R9 model from RNAkinet v1, unpadded architecture
-        'path':'models/rnakinet_r9_5EU_unpad@v1.0',
-        'arch':'rnakinet_r9',
-        'is_unpadded': True
+        'path': 'models/rnakinet_r9_5EU_unpad@v1.0',
+        'arch': 'rnakinet_r9',
+        'is_unpadded': True,
     },
-    
     'rnakinet_r10_5EU': {
         # R10 model from RNAkinet v1, unpadded architecture
-        'path':'models/rnakinet_r10_5EU_unpad@v1.0',
-        'arch':'rnakinet',
-        'is_unpadded': True
+        'path': 'models/rnakinet_r10_5EU_unpad@v1.0',
+        'arch': 'rnakinet',
+        'is_unpadded': True,
     },
 }
