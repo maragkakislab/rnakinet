c1aa94ba8c66f1702d45fe3cdc49738fd4472486
Anirudh Nangunoori <anirudh.nangunoori@nih.gov>
2026-04-02 15:16:44 -0400

    model_loader.py, model_legacy.py: rename model_r9 to the more accurate name model_legacy

--- rnakinet/models/model_loader.py
+++ rnakinet/models/model_loader.py
@@ -1,24 +1,24 @@
 # rename to model_loader.py, move to data_utils/
 from rnakinet.models.model import RNAkinet
 from rnakinet.models.model_experimental import RNAkinet_LastOnly
-from rnakinet.models.model_r9 import RNAkinet as RNAkinet_r9
+from rnakinet.models.model_legacy import RNAkinet as RNAkinet_legacy
 
 arch_map = {'rnakinet': RNAkinet,
             'rnakinet_lastonly': RNAkinet_LastOnly,
-            'rnakinet_r9': RNAkinet_r9
+            'rnakinet_legacy': RNAkinet_legacy
             }
 
 default_models = {
     'rnakinet_r9_5EU': {
         # R9 model from RNAkinet v1, unpadded architecture
         'path': 'models/rnakinet_r9_5EU_unpad@v1.0.ckpt',
-        'arch': 'rnakinet_r9',
+        'arch': 'rnakinet_legacy',
         'pad_reads': False,
     },
     'rnakinet_r10_5EU': {
         # R10 model from RNAkinet v1, unpadded architecture
         'path': 'models/rnakinet_r10_5EU_unpad@v1.0',
-        'arch': 'rnakinet',
+        'arch': 'rnakinet_legacy',
         'pad_reads': False,
     },
 }
